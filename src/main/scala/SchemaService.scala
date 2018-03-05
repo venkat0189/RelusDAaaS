2067ff689e53c39b911be72cea9efa1c276b742a
Bimal Tandel <bimal@MacBook-Pro.local>
2018-03-05 14:40:24 -0500

    initial setup of maven

---
+++ src/main/scala/SchemaService.scala
@@ -0,0 +1,9 @@
+package com.reluscloud.dna.DAaaSUtil
+
+object SchemaService {
+
+  def main(args: Array[String]) {
+    // Current code supports only Eligibility Json data
+    println("Hello World")
+  }
+}
