424ee5d841c0b42c8316319cc23ab3ce68763b8c
LeythRelus <33095263+LeythRelus@users.noreply.github.com>
2018-03-06 11:44:48 -0500

    Updating some Info

--- src/main/scala/SchemaService.scala
+++ src/main/scala/SchemaService.scala
@@ -4,7 +4,7 @@ class SchemaService(val schemaConfigLocation: String) {
 
   var TABLENAME = ""
   var INPUTFOLDER = ""
-  var COLUMNSCHEMA = List[ColumnSchema]
+  var COLUMNSCHEMA = List[ColumnSchema]()
 
 
   def parseSchemaFile {
