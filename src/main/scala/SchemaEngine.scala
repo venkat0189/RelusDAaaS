cdd599b3d84be5fdb3c4a9cc88fe869a4b327fff
LeythRelus <33095263+LeythRelus@users.noreply.github.com>
2018-03-06 13:29:22 -0500

    Added functionality to read each record and apply regex on it

--- src/main/scala/SchemaEngine.scala
+++ src/main/scala/SchemaEngine.scala
@@ -6,6 +6,6 @@ object SchemaEngine {
   def main(args: Array[String]): Unit = {
     // Current code supports only Eligibility Json data
     val schemaEngine = new SchemaService("resources/sample.schema")
-    println("Hello World")
+    schemaEngine.parseSchemaFile
   }
 }
--- src/main/scala/SchemaService.scala
+++ src/main/scala/SchemaService.scala
@@ -1,24 +1,31 @@
 package com.reluscloud.dna.RelusDAaaS.DAaaSUtil
 
+import scala.io.Source
+
 class SchemaService(val schemaConfigLocation: String) {
 
   var TABLENAME = ""
-  var INPUTFOLDER = ""
+  var SOURCEFOLDER = ""
   var COLUMNSCHEMA = List[ColumnSchema]()
+  val Pattern = "^\\{.*\\}$".r
 
 
   def parseSchemaFile {
     try{
-      println(schemaConfigLocation)
-
       this.TABLENAME = "Table Name"
+      this.SOURCEFOLDER = "Input Folder"
 
-      this.INPUTFOLDER = "Input Folder"
+      for (line <- Source.fromFile(schemaConfigLocation).getLines) {
+        var trimmedLine = line.trim
 
-      //for each row, insert into ColumnSchema
-      //bufferedReader.readLine
-      //Parse info on that line
-      //insert into Column Schema
+        trimmedLine match {
+          case Pattern() => {
+            println(trimmedLine)
+          }
+          case _ =>
+        }
+        //Parse each line here
+      }
 
     }catch{
       case e: Exception => println(e)
