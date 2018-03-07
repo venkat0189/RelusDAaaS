6b67dfba02267b230c1493037c892a6dedceec06
Shivangi Shroff <shivangi@REL-22SSHROFF.local>
2018-03-07 10:41:24 -0500

    Object creation

--- src/main/scala/SchemaEngine.scala
+++ src/main/scala/SchemaEngine.scala
@@ -5,7 +5,8 @@ object SchemaEngine {
 
   def main(args: Array[String]): Unit = {
     // Current code supports only Eligibility Json data
-    val schemaEngine = new SchemaService("resources/sample.schema")
-    schemaEngine.parseSchemaFile
+    //val schemaEngine = new SchemaService("resources/sample.schema")
+    val schemaService = new SchemaService("test.schema")
+    schemaService.parseSchemaFile
   }
 }
--- src/main/scala/SchemaService.scala
+++ src/main/scala/SchemaService.scala
@@ -1,34 +1,74 @@
 package com.reluscloud.dna.RelusDAaaS.DAaaSUtil
-
+//import scala.util.control.Breaks._
 import scala.io.Source
 
 class SchemaService(val schemaConfigLocation: String) {
 
   var TABLENAME = ""
-  var SOURCEFOLDER = ""
+  var INPUTFOLDER = ""
+  var OUTPUTFOLDER = ""
+  var DELIMITER =","
   var COLUMNSCHEMA = List[ColumnSchema]()
-  val Pattern = "^\\{.*\\}$".r
+  val Pattern = "^([^:.]*):(.*)$".r
 
+  var columnFound = 0
 
   def parseSchemaFile {
     try{
-      this.TABLENAME = "Table Name"
-      this.SOURCEFOLDER = "Input Folder"
+       for (line <- Source.fromFile(schemaConfigLocation).getLines) {
 
-      for (line <- Source.fromFile(schemaConfigLocation).getLines) {
+        println(line)
+
         var trimmedLine = line.trim
 
-        trimmedLine match {
-          case Pattern() => {
-            println(trimmedLine)
+          trimmedLine match {
+
+            case Pattern(skey, svalue) => {
+              println("key is " + skey.trim)
+              println("value is " + svalue.trim)
+              if (columnFound == 0) {
+                skey.trim match {
+                  case "tableName" => {
+                    this.TABLENAME = svalue.trim
+                  }
+                  case "inputFolder" => {
+                    this.INPUTFOLDER = svalue.trim
+                  }
+                  case "outputFolder" => {
+                    this.OUTPUTFOLDER = svalue.trim
+                  }
+                  case "delimiter" => {
+                    this.DELIMITER = svalue.trim
+                  }
+                  case "columns" => {
+                    columnFound = 1
+
+                  }
+
+                  case _ => {
+                    println("no match -->" + trimmedLine)
+                  }
+                }
+              }
+              if (columnFound == 1) {
+                skey.trim match {
+                  case _ => {
+                    if(skey.trim!='(' || skey.trim!=')'){}
+                    //Add objects to COLUMNSCHEMA list
+                  }
+                }
+              }
+            }
           }
-          case _ =>
-        }
         //Parse each line here
       }
 
     }catch{
       case e: Exception => println(e)
     }
+
+    print(COLUMNSCHEMA)
   }
+
+
 }
