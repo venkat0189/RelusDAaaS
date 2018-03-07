3f75d624fbc8414d5664855bc9dac33a50414820
Shivangi Shroff <shivangi@REL-22SSHROFF.local>
2018-03-07 11:29:19 -0500

    object created with columns

--- src/main/scala/ColumnService.scala
+++ src/main/scala/ColumnService.scala
@@ -1,5 +1,6 @@
 package com.reluscloud.dna.RelusDAaaS.DAaaSUtil
 
 import scala.collection.mutable
+case class ColumnSchema(columnName: String, columnDataType: String)
 
-case class ColumnSchema(columnName: String, columnDataType: String, Attributes: mutable.HashMap[String, String])
+//case class ColumnSchema(columnName: String, columnDataType: String, Attributes: mutable.HashMap[String, String])
--- src/main/scala/SchemaService.scala
+++ src/main/scala/SchemaService.scala
@@ -2,13 +2,17 @@ package com.reluscloud.dna.RelusDAaaS.DAaaSUtil
 //import scala.util.control.Breaks._
 import scala.io.Source
 
+import scala.collection.mutable.ListBuffer
+
+
 class SchemaService(val schemaConfigLocation: String) {
 
   var TABLENAME = ""
   var INPUTFOLDER = ""
   var OUTPUTFOLDER = ""
   var DELIMITER =","
-  var COLUMNSCHEMA = List[ColumnSchema]()
+  var COLUMNSCHEMA = new ListBuffer[ColumnSchema]()
+  //var COLUMNSCHEMA = List[ColumnSchema]()
   val Pattern = "^([^:.]*):(.*)$".r
 
   var columnFound = 0
@@ -17,52 +21,57 @@ class SchemaService(val schemaConfigLocation: String) {
     try{
        for (line <- Source.fromFile(schemaConfigLocation).getLines) {
 
-        println(line)
+         println(line)
 
-        var trimmedLine = line.trim
+         var trimmedLine = line.trim
 
-          trimmedLine match {
+         trimmedLine match {
+           case "(" =>{}
+           case ")" =>{}
+           case Pattern(skey, svalue) => {
+             println("key is " + skey.trim)
+             println("value is " + svalue.trim)
+             if (columnFound == 0) {
+               skey.trim match {
+                 case "tableName" => {
+                   this.TABLENAME = svalue.trim
+                 }
+                 case "inputFolder" => {
+                   this.INPUTFOLDER = svalue.trim
+                 }
+                 case "outputFolder" => {
+                   this.OUTPUTFOLDER = svalue.trim
+                 }
+                 case "delimiter" => {
+                   this.DELIMITER = svalue.trim
+                 }
+                 case "columns" => {
+                   columnFound = 1
 
-            case Pattern(skey, svalue) => {
-              println("key is " + skey.trim)
-              println("value is " + svalue.trim)
-              if (columnFound == 0) {
-                skey.trim match {
-                  case "tableName" => {
-                    this.TABLENAME = svalue.trim
-                  }
-                  case "inputFolder" => {
-                    this.INPUTFOLDER = svalue.trim
-                  }
-                  case "outputFolder" => {
-                    this.OUTPUTFOLDER = svalue.trim
-                  }
-                  case "delimiter" => {
-                    this.DELIMITER = svalue.trim
-                  }
-                  case "columns" => {
-                    columnFound = 1
+                 }
 
-                  }
+                 case _ => {
+                   println("no match -->" + trimmedLine)
+                 }
+               }
+             }
+             else if (columnFound == 1) {
+               skey.trim match {
+                 case _ => {
 
-                  case _ => {
-                    println("no match -->" + trimmedLine)
-                  }
-                }
-              }
-              if (columnFound == 1) {
-                skey.trim match {
-                  case _ => {
-                    if(skey.trim!='(' || skey.trim!=')'){}
-                    //Add objects to COLUMNSCHEMA list
-                  }
-                }
-              }
-            }
-          }
-        //Parse each line here
-      }
+                     //Add objects to COLUMNSCHEMA list
+                     COLUMNSCHEMA += new ColumnSchema(columnName = skey, columnDataType = svalue)
 
+
+
+                   }
+                 }
+               }
+
+           }
+           //Parse each line here
+         }
+       }
     }catch{
       case e: Exception => println(e)
     }
